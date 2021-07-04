cb0224bc17f0b841194983bc743f231d3fcdaa5d
saber2pr <saber2pr@gmail.com>
2021-07-04 13:58:29 +0800

    fix

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -8,7 +8,7 @@ DATE=$(date +%Y%m%d%H%M%S);
 VERSION=$DATE;
 
 # BRANCH
-default_test_branch="testing";
+default_test_branch="master";
 test_branch=${1:-$default_test_branch};
 
 cd /home/${WORKSPACE}/${NAME};
@@ -17,4 +17,4 @@ git pull origin $test_branch;
 docker image build -t ${NAME}:$VERSION . \
 && docker stop ${NAME} \
 && docker rm ${NAME} \
-&& docker run --restart=always --name=${NAME} -d -e NODE_ENV=testing -p ${PORT}:${PORT} ${NAME}:${VERSION};
+&& docker run --restart=always --name=${NAME} -d -e NODE_ENV=production -p ${PORT}:${PORT} ${NAME}:${VERSION};
--- scripts/run.sh
+++ scripts/run.sh
@@ -4,4 +4,4 @@ PORT=443;
 
 docker stop ${NAME} \
 && docker rm ${NAME};
-docker run --restart=always --name=${NAME} -d -e NODE_ENV=testing -p ${PORT}:${PORT} ${NAME}:$1;
+docker run --restart=always --name=${NAME} -d -e NODE_ENV=production -p ${PORT}:${PORT} ${NAME}:$1;
