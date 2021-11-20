cda1f39bdd10c5c6558ded1b3026c156f9daf49a
saber2pr <saber2pr@gmail.com>
2021-11-20 15:55:43 +0800

    chore: update

--- scripts/docker-deploy.sh
+++ scripts/docker-deploy.sh
@@ -1,3 +1,3 @@
 tag=$1
-docker pull saber2pr/next-ssr-blog:$tag
-docker run --restart=always -d -it saber2pr/next-ssr-blog:$tag
+docker pull saber2pr/next-ssr-blog:$tag > ./deploy-blog.log
+docker run --restart=always -d -p 88:3000 -it saber2pr/next-ssr-blog:$tag > ./deploy-blog.log
